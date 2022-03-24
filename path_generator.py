206aced2f7a72a388e87d79f31833217717edd64
Frederik Fuglsang Midtgaard <midtgaard@hotmail.dk>
2022-03-25 00:43:18 +0100

    Shortests path generator done

--- path_generator.py
+++ path_generator.py
@@ -1,64 +1,69 @@
+from distutils.log import fatal
 from queue import PriorityQueue
-from graph import Graph,Node, NodeClass
+from graph import EdgeClass, Graph,Node, NodeClass
 from path import Path
+from marking import Marking
 from abc import ABC, abstractmethod
 
 
 class PathGenerator(ABC):
     current_path = Path()
 
-    def __init__(self, graph: Graph, goalnode: Node, startnode: Node):
-        self.buildPath(graph, goalnode, startnode)
+    def __init__(self, graph: Graph, goalnodeid, startnodeid, marking: Marking):
+        self.buildPath(graph, goalnodeid, startnodeid, marking)
         pass
     
     @abstractmethod
-    def buildPath(self, graph: Graph, goalnode_id, startnode_id):
+    def buildPath(self, graph: Graph, goalnode_id, startnode_id, marking: Marking) -> Path:
         pass
 
     def getFullPath(self):
         return self.current_path
         
-
-    def getStrategy(self, graph: Graph,goalnode,currentnode):
-        if(currentnode not in self.current_path.get_all_nodes()):
-            self.buildPath(graph,goalnode,currentnode)
-        
+    def getStrategy(self, graph: Graph,goalnode,currentnodeid):
+        if(currentnodeid not in self.current_path.get_all_nodes()):
+            self.buildPath(graph,goalnode,currentnodeid)
         return self.getnextRecommendedEdge()
-
     
     def getnextRecommendedEdge(self):
-        return self.current_path.nodes_in_path.pop()
-
+        return self.current_path.path_edges[0]
     
 
 
 class ShortestPathGenerator(PathGenerator):
     
-    def buildPath(self, graph: Graph, goalnode: Node, startnode: Node):
-        self.current_path = self.dijkstra(graph,goalnode,startnode)
+    def buildPath(self, graph: Graph, goalnodeid , startnodeid, marking: Marking) -> Path:
+        self.current_path = self.dijkstra(graph,goalnodeid,startnodeid, marking)
 
-    def dijkstra (self, graph: Graph, goalnode: Node, startnode: Node):
+    def dijkstra (self, graph: Graph, goalnodeid, startnodeid, marking: Marking) -> Path:
         #loosely inspired by https://stackabuse.com/dijkstras-algorithm-in-python/
-        shortest_path = Path()
-        
         frontier = PriorityQueue()
-        frontier.put((0,[startnode]))
-        visited = []
-        #work in progress
+        
+        startedges = graph.get_out_edges(startnodeid,EdgeClass.NORMAL)
+        for edge in startedges:
+            if graph.get_node(edge.to_id).kind == NodeClass.OK:
+                frontier.put((edge.cost,[edge]))
+        
+        visited_nodes = []
         while not frontier.empty():
-            (dist,current_path) = frontier.get()
-            current_node = self.getLastAddedNode(current_path)
-            visited.append(current_node)
+            (dist,current_edge_path) = frontier.get()
+            current_edge = self.getLastAddedEdge(current_edge_path)
+            current_node_id = current_edge.to_id
+            visited_nodes.append(current_node_id)
 
-            for edge in graph.get_out_edges(current_node.id,NodeClass.OK):
+            if current_node_id == goalnodeid:
+                return Path(current_edge_path,graph,marking)
+
+            for edge in graph.get_out_edges(current_node_id, EdgeClass.NORMAL):
                 neighbor_node = graph.get_node(edge.to_id)
                 cost = dist + edge.cost
-                if neighbor_node not in visited:
-                    frontier.put(cost,current_path.append())
+                if neighbor_node.id not in visited_nodes and neighbor_node.kind == NodeClass.OK:
+                    current_edge_path.append(edge)
+                    frontier.put(cost,current_edge_path)
         
-
-        return shortest_path
+        #No route was found
+        return None
     
-    def getLastAddedNode(path: list):
+    def getLastAddedEdge(path: list):
         return path[-1]
 
