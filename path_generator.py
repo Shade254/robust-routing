14d6cfe9dede174bb07b8752739ed7a8e0a3ade1
Frederik Fuglsang Midtgaard <midtgaard@hotmail.dk>
2022-04-06 18:52:23 +0200

    ShortestPath Optimized for performance

--- path_generator.py
+++ path_generator.py
@@ -31,7 +31,7 @@ class PathGenerator(ABC):
         
     
 
-import heapq
+from heapq import heappush, heappop
 class ShortestPathGenerator(PathGenerator):
     def build_path(self, graph: Graph, goalnode_id , startnode_id, marking: Marking) -> Path:
         self.current_path = self.dijkstra(graph,goalnode_id,startnode_id, marking)
@@ -39,31 +39,42 @@ class ShortestPathGenerator(PathGenerator):
     def dijkstra(self, graph: Graph, goal_node_id, startnodeid, marking: Marking) -> Path:
         #loosely inspired by https://stackabuse.com/dijkstras-algorithm-in-python/
         frontier = []
-        
+
         startedges = graph.get_out_edges(startnodeid,EdgeClass.NORMAL)
         for edge in startedges:
             if graph.get_node(edge.to_id).kind == NodeClass.OK:
-                frontier.append((edge.cost,[edge]))
-                
+                start_path = [edge]
+                heappush(frontier, (edge.cost, id(start_path), start_path))
+
+        cost_to_reach_nodes = dict()    
         visited_nodes = []
         while not frontier == []:
-            next_edge_path = min(frontier, key = lambda t: t[0])
-            frontier.remove(next_edge_path)
-            (dist,current_edge_path) = next_edge_path
-
+            print(len(frontier))
+            next_edge_path = heappop(frontier)
+            (dist,id_,current_edge_path) = next_edge_path
             current_edge = self.get_last_added_edge(current_edge_path)
             current_node_id = current_edge.to_id
             visited_nodes.append(current_node_id)
+            cost_to_reach_nodes[current_node_id] = dist
 
             if current_node_id == goal_node_id:
+                print(visited_nodes)
                 return Path(current_edge_path,graph,marking)
 
             for edge in graph.get_out_edges(current_node_id, EdgeClass.NORMAL):
                 neighbor_node = graph.get_node(edge.to_id)
                 cost = dist + edge.cost
-                if (neighbor_node.id not in visited_nodes) and (neighbor_node.kind == NodeClass.OK):
-                    frontier.append((cost,current_edge_path+[edge]))
-        
+                if  (neighbor_node.id not in visited_nodes) and (neighbor_node.kind == NodeClass.OK):
+                    if (neighbor_node.id in cost_to_reach_nodes):
+                        if cost_to_reach_nodes[neighbor_node.id] > cost:
+                            cost_to_reach_nodes[neighbor_node.id] = cost
+                            path_to_neighbor = current_edge_path+[edge]
+                            heappush(frontier, (cost,id(path_to_neighbor),path_to_neighbor))
+                    else :
+                            cost_to_reach_nodes[neighbor_node.id] = cost
+                            path_to_neighbor = current_edge_path+[edge]
+                            heappush(frontier, (cost,id(path_to_neighbor),path_to_neighbor))
+
         #No route was found
         return None
     
