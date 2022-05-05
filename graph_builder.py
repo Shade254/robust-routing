cda044807ff5fef2371844a1535fb0b73c6da445
Frederik Fuglsang Midtgaard <midtgaard@hotmail.dk>
2022-05-05 17:54:37 +0200

    Graph builder only produces connected graphs

--- graph_builder.py
+++ graph_builder.py
@@ -9,16 +9,9 @@ class NeigbourDirection(Enum):
     Left = 3
     Right = 4
 
-#A function that builds a graph from the following inputs: x size, y size, number of fatal nodes, location of output
-#The graph should have a size of x by y
-#The graph is made of the characters '.' and 'X' with 'X' representing a fatal node
-#the graph should be outputtet to a txt file, location of txt file is given as input
-#The graph should have a number of fatal nodes equal to the number of fatal nodes inputted
-#fatal nodes are randomly placed on the graph
 def graph_builder(x_size, y_size, num_of_fatal_nodes, num_of_clusters, output_path):
     graph = []
     fatal_nodes_placed = []
-    num_of_fatal_nodes_placed = 0
     output_file = open(output_path, "w")
     
     for i in range(x_size):
@@ -29,29 +22,53 @@ def graph_builder(x_size, y_size, num_of_fatal_nodes, num_of_clusters, output_pa
     for i in range(num_of_clusters):
         x = np.random.randint(0, x_size)
         y = np.random.randint(0, y_size)
-        graph[x][y] = 'X'
+        graph[x][y] = 'x'
         fatal_nodes_placed.append((x, y))
     
-    counter = 0
-    while(num_of_fatal_nodes_placed < num_of_fatal_nodes):
-        counter += 1
+    while(len(fatal_nodes_placed) < num_of_fatal_nodes):
         random.shuffle(fatal_nodes_placed)
         cluster_node_x,cluster_node_y = fatal_nodes_placed[-1]
         x,y = get_neigbour(graph,cluster_node_x,cluster_node_y,random.choice(list(NeigbourDirection)))
-        if graph[x][y] == 'X':
+        if graph[x][y] == 'x':
             continue
-        graph[x][y] = 'X'
-        fatal_nodes_placed.append((x, y))
-        num_of_fatal_nodes_placed += 1    
-        print(counter)
+        if(all_neigbours_are_fatal(graph,x,y)):
+            continue
+        graph[x][y] = 'x'
+        fatal_nodes_placed.append((x, y)) 
     
-    for i in range(x_size):
-        for j in range(y_size):
-            output_file.write(graph[i][j])
-        output_file.write('\n')
-    output_file.close()
-    return graph
+    #Fill out all the holes in fatal clusters
+    for x in range(x_size):
+        for y in range(y_size):
+            if all_neigbours_are_fatal(graph,x,y) and graph[x][y] != 'x':
+                graph[x][y] = 'x'
+                fatal_nodes_placed.append((x, y))
+ 
+    if graph_is_connected(graph,x_size,y_size,len(fatal_nodes_placed)):
+        for i in range(x_size):
+            for j in range(y_size):
+                output_file.write(graph[i][j])
+            output_file.write('\n')
+        output_file.close()
+        return graph
+    else :
+        return None
 
+
+def all_neigbours_are_fatal(graph, x, y):
+    fatal_neigbours = 0
+    if(not coordinate_is_inside_graph(graph, x-1, y) or (graph[x-1][y] == 'x')):
+            fatal_neigbours += 1
+    if(not coordinate_is_inside_graph(graph, x+1, y) or (graph[x+1][y] == 'x')):
+            fatal_neigbours += 1
+    if(not coordinate_is_inside_graph(graph, x, y-1) or (graph[x][y-1] == 'x')):
+            fatal_neigbours += 1
+    if(not coordinate_is_inside_graph(graph, x, y+1) or (graph[x][y+1] == 'x')):
+            fatal_neigbours += 1
+    
+    if(fatal_neigbours == 4):
+        return True
+    else:
+        return False
 
 def get_neigbour(graph, x, y, direction):
     if(direction == NeigbourDirection.Up):
@@ -83,22 +100,76 @@ def coordinate_is_inside_graph(graph, x, y):
         return False
     return True
 
-def neighbouring_nodes_is_fatal(graph, x, y):
-    if(graph[x][y] == 'X'):
+
+#A function that returns true if all non fatal nodes in a graph is interreachable from any other non fatal node
+def graph_is_connected(graph,x_size,y_size,fatal_nodes):
+    x,y = get_random_node(graph,x_size,y_size)
+    visited_nodes = [(x,y)]
+    frontier = [(x,y)]
+    while(len(frontier) > 0):
+        x,y = frontier.pop(0)
+        neighbours = [(-1,0),(1,0),(0,-1),(0,1)]
+        for neigbour in neighbours:
+            i = x+neigbour[0]
+            j = y+neigbour[1]
+            if(not coordinate_is_inside_graph(graph,i,j)):
+                continue
+            if(graph[i][j] == 'x'):
+                continue
+            if(i,j) in visited_nodes:
+                continue
+            visited_nodes.append((i,j))
+            frontier.append((i,j))
+    if len(visited_nodes) == (x_size*y_size)-fatal_nodes:
         return True
-    if(graph[x-1][y] == 'X'):
-        return True
-    if(graph[x+1][y] == 'X'):
-        return True
-    if(graph[x][y-1] == 'X'):
-        return True
-    if(graph[x][y+1] == 'X'):
-        return True
-    return False
+    else: 
+        return False
 
 
+   
+def get_random_node(graph,x_size,y_size):
+    x,y = None,None
+    while x == None and y == None:
+        x = np.random.randint(0, x_size)
+        y = np.random.randint(0, y_size)
+        if(graph[x][y] == 'x'):
+            x,y = None,None
+    return x,y
 
+def count_fatal_nodes(graph,x_size,y_size):
+    count = 0
+    for i in range(x_size):
+        for j in range(y_size):
+            if graph[i][j] == 'x':
+                count += 1
+    return count
+
+def graph_x_size(graph):
+    return len(graph)
+
+def graph_y_size(graph):
+    return len(graph[0])
+
+#function that converts txt file to graph
+def txt_to_graph(txt_path):
+    graph = []
+    file = open(txt_path, "r")
+    for line in file:
+        graph.append(list(line.strip()))
+    file.close()
+    return graph
+
+"""
+Graphs are only created if, the graph is connected. If not, the function will return None.
+Below is an example of genereating a number of graphs.
 
 for i in range(5):
     print(f'creating graph {i}')
-    graph_builder(40,60,250,5,f'test_cases/test_case_{i}.txt')
+    counter = 1
+    while(True):
+        print(f'try {counter}')
+        graph = graph_builder(40,60,250,5,f'test_cases/test_case_{i}.txt') 
+        counter += 1
+        if(graph != None):
+            break
+"""
