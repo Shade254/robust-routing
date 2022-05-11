773f64ec139ee3b99431509def0cfcfca5ed0fd2
Frederik Fuglsang Midtgaard <midtgaard@hotmail.dk>
2022-05-11 16:50:21 +0200

    abs marking and planned based on all runs

--- test_processor.py
+++ test_processor.py
@@ -34,47 +34,63 @@ def group_df_by_column(df, column_name, reset_index=True, drop_column=True):
 
     return groups_dict
     
-output_columns = columns=['strategy','success_rate','increase_in_planned_distance','increase_in_distance_with_wind','planned_robustness_increase','robustness_increase_with_wind']
+output_columns = columns=['strategy','success_rate','increase_in_planned_distance','increase_in_distance_with_wind','planned_robustness','robustness_with_wind','planned_marking_increase','executed_marking_increase']
 df_output = pd.DataFrame(columns=output_columns)
 
-#df = read_csv("DynamicProgrammingStrategy_May_10_2022_18_03.csv")
 df = read_csv("strategies_results.csv")
 
 grouped_df_dict = group_df_by_column(df,'Function')
 print(get_row(grouped_df_dict, 'ShortestLength', 1))
 
+#TODO
+#add absolute value of planned marking and excecuted marking 
+#planned path and planned marking includes unsuccesfull 
+
+
 for name, group in grouped_df_dict.items():
     print(name)
     print(group)
     succes_rate = group['Success'].value_counts(normalize=True).mul(100).iloc[0].astype(str)+'%'
+    
     increase_in_length = 0
     executed_increase_in_length = 0
-    avg_planned_marking = 0
-    avg_executed_marking = 0
+    avg_planned_marking_abs = 0
+    avg_executed_marking_abs = 0
+    avg_planned_marking_inc = 0
+    avg_executed_marking_inc = 0
+    
     num_of_succesfull_runs = 0
+
     for index, row in group.iterrows():
-        if row['Success'] == True:
-            num_of_succesfull_runs += 1
-        else :
-            continue
         shortest_path_data = get_row(grouped_df_dict, 'ShortestLength', index)
-        
         shortest_path_length = ut.get_number_of_pairs(shortest_path_data['PlannedPath'])
+
         planned_path_length = ut.get_number_of_pairs(row['PlannedPath'])
         executed_path_length = ut.get_number_of_pairs(row['ExecutedPath'])
         increase_in_length += (planned_path_length - shortest_path_length)/shortest_path_length
-        executed_increase_in_length += (executed_path_length - shortest_path_length)/shortest_path_length
+        
+        avg_planned_marking_abs += ut.get_avg_marking(row['PlannedPathMarking'])
 
         shortest_path_marking = ut.get_avg_marking(shortest_path_data['PlannedPathMarking'])
-        avg_planned_marking += ut.get_avg_marking(row['PlannedPathMarking'])-shortest_path_marking
-        avg_executed_marking += ut.get_avg_marking(row['ExecutedPathMarking'])-shortest_path_marking
+        avg_planned_marking_inc += ut.get_avg_marking(row['PlannedPathMarking'])-shortest_path_marking
 
-    increase_in_length = round(increase_in_length/num_of_succesfull_runs,2)
+        if row['Success'] == True:
+            num_of_succesfull_runs += 1
+            executed_increase_in_length += (executed_path_length - shortest_path_length)/shortest_path_length
+            avg_executed_marking_abs += ut.get_avg_marking(row['ExecutedPathMarking'])
+            avg_executed_marking_inc += ut.get_avg_marking(row['ExecutedPathMarking'])-shortest_path_marking
+      
+
+    increase_in_length = round(increase_in_length/num_of_elements(group),2)
     executed_increase_in_length = round(executed_increase_in_length/num_of_succesfull_runs,2)
-    avg_planned_marking = round(avg_planned_marking/num_of_succesfull_runs,3)
-    avg_executed_marking = round(avg_executed_marking/num_of_succesfull_runs, 3)
 
-    df_output.loc[len(df_output), df_output.columns] = [name, succes_rate, ut.get_percentage(increase_in_length), ut.get_percentage(executed_increase_in_length), avg_planned_marking, avg_executed_marking]
+    avg_planned_marking_abs = round(avg_planned_marking_abs/num_of_elements(group),3)
+    avg_executed_marking_abs  = round(avg_executed_marking_abs/num_of_succesfull_runs,3)
+
+    avg_planned_marking_inc = round(avg_planned_marking_inc/num_of_elements(group),3)
+    avg_executed_marking_inc = round(avg_executed_marking_inc/num_of_succesfull_runs, 3)
+
+    df_output.loc[len(df_output), df_output.columns] = [name, succes_rate, ut.get_percentage(increase_in_length), ut.get_percentage(executed_increase_in_length), avg_planned_marking_abs, avg_executed_marking_abs, avg_planned_marking_inc, avg_executed_marking_inc]
 
 save_csv(df_output, "output2.csv")
 print(df_output)
